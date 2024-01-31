3d8fd10eebac487500fd165cd4a2ab694554ca0b
Leandro Nicolás Borquez Zuloaga <borquezln@hotmail.com>
2024-01-30 22:07:31 -0300

    Data and state

--- 05 - React Basics/creating_components/src/App.js
+++ 05 - React Basics/creating_components/src/App.js
@@ -1,13 +1,16 @@
-import "./App.css";
-import Card from "./Card";
-
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number'); 
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+  
   return (
-    <div className="App">
-      <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+
+      <button onClick={handleClick}>Adivine el número entre 1 y 3</button>
     </div>
   );
 }
